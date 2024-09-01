2fab335088b3aadaf1e50dfe22ff48a4bdd9a214
Tom Silver <tomssilver@gmail.com>
2024-09-01 10:35:07 -0400

    wip

--- repositioning/dynamics/pybullet_constraint_model.py
+++ repositioning/dynamics/pybullet_constraint_model.py
@@ -12,8 +12,8 @@ class PybulletConstraintRepositioningDynamicsModel(RepositioningDynamicsModel):
         super().__init__(*args, **kwargs)
 
         tf = multiply_poses(
-            self._active_arm.get_end_effector_pose(),
-            self._passive_arm.get_end_effector_pose().invert(),
+            self._active_arm.get_end_effector_pose().invert(),
+            self._passive_arm.get_end_effector_pose(),
         )
         p.createConstraint(
             self._active_arm.robot_id,
@@ -30,6 +30,16 @@ class PybulletConstraintRepositioningDynamicsModel(RepositioningDynamicsModel):
         )
 
     def step(self, torque: list[float]) -> None:
-        import ipdb
-
-        ipdb.set_trace()
+        # TODO: move this into pybullet helpers.
+        p.setJointMotorControlArray(
+            self._active_arm.robot_id,
+            self._active_arm.arm_joints,
+            p.TORQUE_CONTROL,
+            forces=torque,
+            physicsClientId=self._active_arm.physics_client_id,
+        )
+        t = 0.0
+        pybullet_dt = 1.0 / 240
+        while t <= self._dt:
+            p.stepSimulation(physicsClientId=self._active_arm.physics_client_id)
+            t += pybullet_dt
