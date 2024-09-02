1da12e725a43f4c7d540a8be0f8a61cd7cc87b84
Tom Silver <tomssilver@gmail.com>
2024-09-02 17:59:22 -0400

    some fixes

--- repositioning/dynamics/pybullet_constraint_model.py
+++ repositioning/dynamics/pybullet_constraint_model.py
@@ -30,42 +30,6 @@ class PybulletConstraintRepositioningDynamicsModel(RepositioningDynamicsModel):
             childFrameOrientation=tf.orientation,
             physicsClientId=self._active_arm.physics_client_id,
         )
-        # Need to disable default velocity control to use torque control.
-        for robot in [self._active_arm, self._passive_arm]:
-            p.setJointMotorControlArray(
-                robot.robot_id,
-                robot.arm_joints,
-                p.VELOCITY_CONTROL,
-                forces=np.zeros(len(robot.arm_joints)),
-                physicsClientId=robot.physics_client_id,
-            )
-            for joint in robot.arm_joints:
-                # Turn on torque sensor.
-                p.enableJointForceTorqueSensor(
-                    robot.robot_id, joint, 1, physicsClientId=robot.physics_client_id
-                )
-
-                # Remove any joint friction.
-                p.changeDynamics(
-                    robot.robot_id,
-                    joint,
-                    jointDamping=0.0,
-                    anisotropicFriction=0.0,
-                    maxJointVelocity=5000,
-                    linearDamping=0.0,
-                    angularDamping=0.0,
-                    lateralFriction=0.0,
-                    spinningFriction=0.0,
-                    rollingFriction=0.0,
-                    contactStiffness=0.0,
-                    contactDamping=0.0,
-                    physicsClientId=robot.physics_client_id,
-                )
-
-                # Disable any possible collisions.
-                p.setCollisionFilterGroupMask(
-                    robot.robot_id, joint, 0, 0, physicsClientId=robot.physics_client_id
-                )
 
     def step(self, torque: list[float]) -> None:
         # TODO: move this into pybullet helpers.
--- repositioning/main.py
+++ repositioning/main.py
@@ -138,6 +138,47 @@ def _main(scenario: str, dynamics: str, make_video: bool, video_dt: float) -> No
     active_arm, passive_arm, torque_fn, camera_kwargs = _create_scenario(scenario)
     dynamics_model = _create_dynamics_model(dynamics, active_arm, passive_arm, dt)
 
+    # Need to disable default velocity control to use torque control.
+    for robot in [active_arm, passive_arm]:
+        p.setJointMotorControlArray(
+            robot.robot_id,
+            robot.arm_joints,
+            p.VELOCITY_CONTROL,
+            forces=np.zeros(len(robot.arm_joints)),
+            physicsClientId=robot.physics_client_id,
+        )
+        for joint in robot.arm_joints:
+            # Turn on torque sensor.
+            p.enableJointForceTorqueSensor(
+                robot.robot_id, joint, 1, physicsClientId=robot.physics_client_id
+            )
+
+            # Remove any joint friction.
+            p.changeDynamics(
+                robot.robot_id,
+                joint,
+                jointDamping=0.0,
+                anisotropicFriction=0.0,
+                maxJointVelocity=5000,
+                linearDamping=0.0,
+                angularDamping=0.0,
+                lateralFriction=0.0,
+                spinningFriction=0.0,
+                rollingFriction=0.0,
+                contactStiffness=0.0,
+                contactDamping=0.0,
+                physicsClientId=robot.physics_client_id,
+            )
+
+            # Disable any possible collisions.
+            p.setCollisionFilterGroupMask(
+                robot.robot_id, joint, 0, 0, physicsClientId=robot.physics_client_id
+            )
+
+        # Let the simulation settle.
+        for i in range(1000):
+            p.stepSimulation(physicsClientId=robot.physics_client_id)
+
     if make_video:
         imgs = [capture_image(active_arm.physics_client_id, **camera_kwargs)]
 
